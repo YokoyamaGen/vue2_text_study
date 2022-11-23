a38aefeef50950496f758274787e094fda31379c
YokoyamaGen <g.yokoyama.0616@gmail.com>
2022-11-24 08:27:25 +0900

    単一コンポーネントに関する学習

--- chap04/js/prop_basic.js
+++ chap04/js/prop_basic.js
@@ -1,14 +1,20 @@
-Vue.component('my-hello', {
-  props: [ 'yourName' ],
+// const { useDebugValue } = require("react");
+
+Vue.component("my-hello", {
+  props: ["yourName"],
 
   props: {
     yourName: {
       type: String,
-      required: true
-    }
+      // default: '名無しの権兵衛',
+      required: true,
+      validator: function(value) {
+        return value.length <= 5;
+      }
+    },
   },
   template: `<div>こんにちは、{{ yourName }}さん！</div>`,
-})
+});
 
 new Vue({
   el: '#app',
